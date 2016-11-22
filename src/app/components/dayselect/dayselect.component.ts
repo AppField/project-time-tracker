aa29dfa5baad5a5ce3e1841e4965083e524a6223
WebField <roman@muehlfeldner.net>
2016-11-22 15:34:18 +0100

    Styled/animated dayselect component.

--- src/app/components/dayselect/dayselect.component.ts
+++ src/app/components/dayselect/dayselect.component.ts
@@ -1,14 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, HostBinding} from '@angular/core';
+import { NgbDatepickerConfig, NgbDateStruct } from '@ng-bootstrap/ng-bootstrap';
+import fullyResolved = webdriver.promise.fullyResolved;
+
 
 @Component({
   selector: 'ptc-dayselect',
   templateUrl: 'dayselect.component.html',
-  styleUrls: ['dayselect.component.css']
+  styleUrls: ['dayselect.component.css'],
+  providers: [NgbDatepickerConfig]
 })
-export class DayselectComponent implements OnInit {
+export class DayselectComponent implements OnInit  {
+  model:  NgbDateStruct;
 
-  constructor() { }
+  @HostBinding('class.fullsize') fullsizeMode = false;
 
+  constructor(config: NgbDatepickerConfig) {
+    // config.outsideDays = 'hidden';f
+  }
+
+  enableFullsize() {
+    this.fullsizeMode = !this.fullsizeMode;
+    console.log(this.fullsizeMode);
+  }
+
+  isWeekend(date: NgbDateStruct) {
+    const d = new Date(date.year, date.month - 1, date.day);
+    return d.getDay() === 0 || d.getDay() === 6;
+  }
+
+  isDisabled(date: NgbDateStruct, current: {month: number}) {
+    return date.month !== current.month;
+  }
   ngOnInit() {
   }
 
