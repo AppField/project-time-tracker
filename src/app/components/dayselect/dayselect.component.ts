d2d8142ae5dd51c597eee6b6b90ea9f7f3bac2cf
WebField <roman@muehlfeldner.net>
2016-12-09 13:27:40 +0100

    dayselect: fixed closing of datepicker.

--- src/app/components/dayselect/dayselect.component.ts
+++ src/app/components/dayselect/dayselect.component.ts
@@ -1,6 +1,5 @@
-import {Component, OnInit, HostBinding, HostListener, ElementRef, ViewChild} from '@angular/core';
+import {Component, OnInit, HostBinding, HostListener, ElementRef} from '@angular/core';
 import {NgbDatepickerConfig, NgbDateStruct} from '@ng-bootstrap/ng-bootstrap';
-import {NgbDatepicker} from "@ng-bootstrap/ng-bootstrap/datepicker/datepicker";
 
 
 @Component({
@@ -10,10 +9,10 @@ import {NgbDatepicker} from "@ng-bootstrap/ng-bootstrap/datepicker/datepicker";
   providers: [NgbDatepickerConfig]
 })
 export class DayselectComponent implements OnInit {
-  model: NgbDateStruct;
+  selectedDay: NgbDateStruct;
 
   private offsetTop: number;
-  @HostBinding('class.fullsize') fullsizeMode = false;
+  @HostBinding('class.fullsize') fullsize = false;
   @HostBinding('class.sticky') sticky = false;
 
   @HostListener('window:scroll', ['$event'])
@@ -24,17 +23,47 @@ export class DayselectComponent implements OnInit {
 
   constructor(elementRef: ElementRef) {
     this.offsetTop = elementRef.nativeElement.offsetTop;
+    let today = new Date();
+    this.selectedDay = { day: today.getDate(), month: today.getMonth() +1, year: today.getFullYear() };
   }
 
-  enableFullsize(): void {
-    this.fullsizeMode = !this.fullsizeMode;
+
+  toggleFullsize(): void {
+    this.fullsize = !this.fullsize;
     // TODO: replace with angular-way method: hide scrollbars.
-    if (this.fullsizeMode) {
+    if (this.fullsize) {
       document.getElementsByTagName("body")[0].style.overflow = 'hidden';
     }
     else {
       document.getElementsByTagName("body")[0].style.overflow = 'auto';
     }
+  }
+
+  nextDay(): void {
+    let newDate = new Date();
+    newDate.setMonth(this.selectedDay.month -1);
+    newDate.setFullYear(this.selectedDay.year);
+    newDate.setDate(this.selectedDay.day + 1);
+
+    this.selectedDay = { day: newDate.getDate(), month: newDate.getMonth() +1, year: newDate.getFullYear() };
+  }
+
+  previousDay(): void {
+    let newDate = new Date();
+    newDate.setMonth(this.selectedDay.month -1);
+    newDate.setFullYear(this.selectedDay.year);
+    newDate.setDate(this.selectedDay.day -1);
+
+    this.selectedDay = { day: newDate.getDate(), month: newDate.getMonth() +1, year: newDate.getFullYear() };
+  }
+
+  isWeekend(date: NgbDateStruct) {
+    const d = new Date(date.year, date.month - 1, date.day);
+    return d.getDay() === 0 || d.getDay() === 6;
+  }
+
+  isDisabled(date: NgbDateStruct, current: {month: number}) {
+    return date.month !== current.month;
   }
 
   ngOnInit() {
